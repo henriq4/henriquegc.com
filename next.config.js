6b6719ac50d5ae79d9feb5d855096fa3fc6648e7
Henrique GC <henriq4net@gmail.com>
2023-01-23 17:46:49 -0300

    refactor: implement experimental appdir nextjs feature

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
