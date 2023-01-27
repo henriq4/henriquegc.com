0839d02c4b5a699f5688b83c9d15227215dbd990
Henrique GC <henriq4net@gmail.com>
2023-01-27 18:44:43 -0300

    chore: initial blog page fetching notion cms database

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { notionClient } from 'lib/config/notion';
+import { blockQueryObject } from 'lib/models/notion';
+
+const fetchNotion = async () => {
+  // const { results: databaseQueryResultsRaw } =
+  //   await notionClient.databases.query({
+  //     database_id: databaseId,
+  //   });
+
+  // const databasePages = await notionClient.pages.retrieve({
+  //   page_id: pageId,
+  // });
+
+  const { results: blockQueryResultsRaw } =
+    await notionClient.blocks.children.list({
+      block_id: 'cad8309c-2e58-476a-8fa0-9203f8e106bd',
+    });
+
+  const blockQueryResultsParsed: blockQueryObject[] = blockQueryResultsRaw.map(
+    blockContent => {
+      // @ts-ignore
+      const { type, id } = blockContent;
+      const content = blockContent[type];
+
+      return {
+        id,
+        type,
+        content,
+      };
+    },
+  );
+
+  return blockQueryResultsParsed;
+};
+
+export default async function Page() {
+  const data = await fetchNotion();
+
+  return (
+    <div className="h-screen flex justify-center items-center flex-col">
+      {data.map(e => {
+        return (
+          <h1 key={e.id} className="block">
+            {e.type}
+          </h1>
+        );
+      })}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,7 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import { notionClient } from 'lib/config/notion';
-import { blockQueryObject } from 'lib/models/notion';
-
-const fetchNotion = async () => {
-  // const { results: databaseQueryResultsRaw } =
-  //   await notionClient.databases.query({
-  //     database_id: databaseId,
-  //   });
-
-  // const databasePages = await notionClient.pages.retrieve({
-  //   page_id: pageId,
-  // });
-
-  const { results: blockQueryResultsRaw } =
-    await notionClient.blocks.children.list({
-      block_id: 'cad8309c-2e58-476a-8fa0-9203f8e106bd',
-    });
-
-  const blockQueryResultsParsed: blockQueryObject[] = blockQueryResultsRaw.map(
-    blockContent => {
-      // @ts-ignore
-      const { type, id } = blockContent;
-      const content = blockContent[type];
-
-      return {
-        id,
-        type,
-        content,
-      };
-    },
-  );
-
-  return blockQueryResultsParsed;
-};
-
 export default async function Page() {
-  const data = await fetchNotion();
-
   return (
     <div className="h-screen flex justify-center items-center">
-      {data.map(e => {
-        return (
-          <h1 key={e.id} className="block">
-            {e.type}
-          </h1>
-        );
-      })}
+      <h1>News comming soon...</h1>
     </div>
   );
 }
