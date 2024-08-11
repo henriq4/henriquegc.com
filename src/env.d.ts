58a8ee202df570739bb09725da3dea1e66819a55
Henrique GC <henriq4net@gmail.com>
2024-08-11 20:16:24 -0300

    chore: config alpine, tailwind and tsconfig

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,6 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+  Alpine: import("alpinejs").Alpine;
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1224px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
